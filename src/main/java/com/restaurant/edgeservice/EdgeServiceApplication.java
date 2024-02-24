202cb71c98a2f88c76cb5ad40b55d3babdcb1d53
dreevo <sidedark85@gmail.com>
2024-02-24 19:08:06 +0100

    routes for spa and keycloak

--- src/main/java/com/restaurant/edgeservice/EdgeServiceApplication.java
+++ src/main/java/com/restaurant/edgeservice/EdgeServiceApplication.java
@@ -2,10 +2,9 @@ package com.restaurant.edgeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
-@ConfigurationPropertiesScan
+@SpringBootApplication(exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
 public class EdgeServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/restaurant/edgeservice/web/WebEndpoints.java
+++ src/main/java/com/restaurant/edgeservice/web/WebEndpoints.java
@@ -1,5 +1,6 @@
 package com.restaurant.edgeservice.web;
 
+import reactor.core.publisher.Mono;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,17 +8,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Mono;
 
 @Configuration
 public class WebEndpoints {
 
-
-    @Bean
-    public RouterFunction<ServerResponse> routerFunction() {
-        return RouterFunctions.route().GET("tasty-fallback",
-                        request -> ServerResponse.ok().body(Mono.just(""), String.class))
-                .POST("tasty-fallback",
-                        request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).build()).build();
-    }
+	@Bean
+	public RouterFunction<ServerResponse> routerFunction() {
+		return RouterFunctions.route()
+				.GET("/tasty-fallback", request ->
+						ServerResponse.ok().body(Mono.just(""), String.class))
+				.POST("/tasty-fallback", request ->
+						ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).build())
+				.build();
+	}
+	
 }
